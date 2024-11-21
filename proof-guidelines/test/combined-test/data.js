7d552c7983270b6fc63e9a6433caef3fe701457c
LegoFigure11 <24732684+LegoFigure11@users.noreply.github.com>
2024-11-22 01:48:51 +1100

    Add proofing combined test page

---
+++ proof-guidelines/test/combined-test/data.js
@@ -0,0 +1,160 @@
+var data = {
+  Switch: {
+    Scarlet: {
+      gen: 9,
+      enc: ["Wild", "Static", "Gift", "Raid"],
+    },
+    Violet: {
+      gen: 9,
+      enc: ["Wild", "Static", "Gift", "Raid"],
+    },
+    "Legends: Arceus": {
+      gen: "8a",
+      enc: ["Static", "Gift", "Outbreak", "Spawner"],
+    },
+    "Brilliant Diamond": {
+      gen: "8b",
+      enc: ["Wild", "Static", "Gift", "Pok\u{00e9}Radar"],
+    },
+    "Shining Pearl": {
+      gen: "8b",
+      enc: ["Wild", "Static", "Gift", "Pok\u{00e9}Radar"],
+    },
+    Sword: {
+      gen: 8,
+      enc: ["Wild", "Static", "Gift", "Raid"],
+    },
+    Shield: {
+      gen: 8,
+      enc: ["Wild", "Static", "Gift", "Raid"],
+    },
+    "Let's GO! Pikachu": {
+      gen: "7b",
+      enc: ["All encounter types"],
+    },
+    "Let's GO! Eevee": {
+      gen: "7b",
+      enc: ["All encounter types"],
+    },
+  },
+  "3DS": {
+    "Ultra Sun": {
+      gen: 7,
+      enc: ["Wild", "Static", "Egg", "Gift", "SOS"],
+    },
+    "Ultra Moon": {
+      gen: 7,
+      enc: ["Wild", "Static", "Egg", "Gift", "SOS"],
+    },
+    Sun: {
+      gen: 7,
+      enc: ["Wild", "Static", "Egg", "Gift", "SOS"],
+    },
+    Moon: {
+      gen: 7,
+      enc: ["Wild", "Static", "Egg", "Gift", "SOS"],
+    },
+    "Omega Ruby": {
+      gen: 6,
+      enc: ["Wild", "Static", "Egg", "Gift"],
+    },
+    "Alpha Sapphire": {
+      gen: 6,
+      enc: ["Wild", "Static", "Egg", "Gift"],
+    },
+    X: {
+      gen: 6,
+      enc: ["Wild", "Static", "Egg", "Gift"],
+    },
+    Y: {
+      gen: 6,
+      enc: ["Wild", "Static", "Egg", "Gift"],
+    },
+  },
+  DS: {
+    "Black 2": {
+      gen: 5,
+      enc: ["Wild", "Static", "Egg", "Gift"],
+    },
+    "White 2": {
+      gen: 5,
+      enc: ["Wild", "Static", "Egg", "Gift"],
+    },
+    Black: {
+      gen: 5,
+      enc: ["Wild", "Static", "Egg", "Gift"],
+    },
+    White: {
+      gen: 5,
+      enc: ["Wild", "Static", "Egg", "Gift"],
+    },
+    HeartGold: {
+      gen: 4,
+      enc: ["Wild", "Static", "Egg", "Gift"],
+    },
+    SoulSilver: {
+      gen: 4,
+      enc: ["Wild", "Static", "Egg", "Gift"],
+    },
+    Platinum: {
+      gen: 4,
+      enc: ["Wild", "Static", "Egg", "Gift"],
+    },
+    Diamond: {
+      gen: 4,
+      enc: ["Wild", "Static", "Egg", "Gift"],
+    },
+    Pearl: {
+      gen: 4,
+      enc: ["Wild", "Static", "Egg", "Gift"],
+    },
+  },
+  GBA: {
+    Emerald: {
+      gen: 3,
+      enc: ["Wild", "Static", "Egg", "Gift"],
+    },
+    FireRed: {
+      gen: 3,
+      enc: ["Wild", "Static", "Egg", "Gift"],
+    },
+    LeafGreen: {
+      gen: 3,
+      enc: ["Wild", "Static", "Egg", "Gift"],
+    },
+    Ruby: {
+      gen: 3,
+      enc: ["Wild", "Static", "Egg", "Gift"],
+    },
+    Sapphire: {
+      gen: 3,
+      enc: ["Wild", "Static", "Egg", "Gift"],
+    },
+  },
+  GameCube: {
+    Colosseum: {
+      gen: "cxd",
+      enc: ["Encounter", "Gift"],
+    },
+    XD: {
+      gen: "cxd",
+      enc: ["Encounter", "Gift"],
+    },
+    "Bonus Disc": {
+      gen: "gcbd",
+      enc: ["All Bonus Discs"],
+    },
+  },
+  "Virtual Console": {
+    "All VC Titles": {
+      gen: "vc",
+      enc: ["Wild", "Egg", "Gift"],
+    },
+  },
+  Mobile: {
+    "GO!": {
+      gen: "go",
+      enc: ["All transfers"],
+    },
+  },
+};
