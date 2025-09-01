5702f831f7ede7075b8fa4e77bdf3df20bbe5e82
LegoFigure11 <24732684+LegoFigure11@users.noreply.github.com>
2025-09-01 21:57:47 +1000

    Add OGA gallery test

---
+++ misc/gallery/oga-archive/userdata.js
@@ -0,0 +1,20 @@
+var userdata = {
+  chimera: {
+    display_name: "Chimera",
+    class_name: "chimera",
+    social_name: "Smogon Forums",
+    social_link: "https://www.smogon.com/forums/threads/3691022/",
+  },
+  ciran: {
+    display_name: "Ciran",
+    class_name: "ciran",
+    social_name: "Smogon Forums",
+    social_link: "https://www.smogon.com/forums/threads/3577821/",
+  },
+  dawolfkid: {
+    display_name: "DaWolfKid",
+    class_name: "dawolfkid",
+    social_name: "Smogon Forums",
+    social_link: "https://www.smogon.com/forums/threads/3720593/",
+  },
+};
